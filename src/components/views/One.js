8bbb59f05c3edf273a3524518b103125121322b4
migo98ve <71774873+migo98ve@users.noreply.github.com>
2023-01-26 14:03:22 -0400

    changes
    changes

--- src/components/views/One.js
+++ src/components/views/One.js
@@ -1,46 +1,116 @@
 import React, { useState, useEffect } from "react";
-import Pagination from 'react-bootstrap/Pagination';
 import axios from "axios";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Paginate from './Paginate';
+import Form from 'react-bootstrap/Form';
+
+
+import ModalOne from "../modals/ModalOne";
 const baseURL = "https://dummyjson.com/products";
 
-let active = 2;
-let items = [];
-for (let number = 1; number <= 5; number++) {
-    items.push(
-        <Pagination.Item key={number} active={number === active}>
-            {number}
-        </Pagination.Item>,
-    );
-}
+
 
 function One() {
     const [products, setProducts] = useState([]);
+    const [cardNumbers, setCardNumbers] = useState('col-3 pt-5 mx-auto');
+    const [showProduct, setShowProduct] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [prodsPerPage, setProdsPerPage] = useState(8);
+    const [modalShow, setModalShow] = useState(false);
+
+
+    const getProducts = async () => {
+        axios.get(baseURL).then((response) => prod(response.data))
+            .catch((error) => console.error('this is the error', error))
+    }
+
+    const prod = (json) => {
+        console.log(json)
+        setProducts(json['products'])
+    }
+
 
     useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            console.log('productos', response)
-            setProducts(response.data);
-        });
+        getProducts()
     }, []);
+
+    const indexOfLastPost = currentPage * prodsPerPage;
+    const indexOfFirstPost = indexOfLastPost - prodsPerPage;
+    const currentProds = products.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const previousPage = () => {
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const nextPage = () => {
+        if (currentPage !== Math.ceil(products.length / prodsPerPage)) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const cardsQuantity = (e) => {
+        console.log(e)
+        const quantity = (12/e)
+        console.log(quantity)
+        setCardNumbers('col-'+quantity +' pt-5 mx-auto')
+        setProdsPerPage((e * 2))
+    };
+
+
     return (
+
         <div style={{ justifyContent: 'none' }} className="App-header">
-
-            <p>
-                HELLOO challenge ONEEEEEEEE
-            </p>
-            <div>
-                <Pagination>{items}</Pagination>
-                <br />
-
-                <Pagination size="lg">{items}</Pagination>
-                <br />
-
-                <Pagination size="sm">{items}</Pagination>
+            <div className="row">
+            <Form.Select onChange={e => cardsQuantity(e.target.value)} aria-label="Default select example">
+                <option>Cantidad cartas por fila</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="6">6</option>
+                <option value="12">12</option>
+            </Form.Select>
             </div>
-
+           
+            <div className="row mx-auto" style={{ paddingLeft: '50px', paddingRight: '50px' }}>
+                {currentProds.map((products) => (
+                    <div className={cardNumbers} key={products.id}>
+                        <Card onClick={() => { setModalShow(true); setShowProduct(products) }} className="h-100 card" style={{ color: 'black', backgroundColor: '#8f9090' }}>
+                            <Card.Img style={{ height: '250px' }} variant="top" src={products.thumbnail} />
+                            <Card.Body>
+                                <Card.Title className="text-center">{products.brand} - {products.title}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                ))}
+            </div>
+            <Paginate
+                prodsPerPage={prodsPerPage}
+                totalProds={products.length}
+                currentPage={currentPage}
+                paginate={paginate}
+                previousPage={previousPage}
+                nextPage={nextPage}
+            />
+            <ModalOne
+                id={showProduct}
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </div>
     );
 }
+
+/* ReactDOM.render(
+    <One itemsPerPage={4} />,
+    document.getElementById('container')
+  ); */
 
 
 
